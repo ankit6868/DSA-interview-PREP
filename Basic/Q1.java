d659368afeb1fd5da3288a7e77bf78397bdb9d8b
Ankit khanda <60369835+ankit6868@users.noreply.github.com>
2024-01-18 12:52:36 +0530

    q1 chg

--- Basic/Q1.java
+++ Basic/Q1.java
@@ -1,7 +1,7 @@
 // 1) Write a Program to print "Hello, World!"
 
 class HelloWorld {
-    public static void main(String[]args){
-        system.out.print("hello");
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
 }
